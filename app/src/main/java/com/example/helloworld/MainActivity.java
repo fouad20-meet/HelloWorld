6ddf5638e3cff93549ff511a13c6dee08acaa6bd
Fouad Haddad <54176589+fouadhaddad@users.noreply.github.com>
2023-02-06 15:21:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView title;
+    private EditText food;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        title = findViewById(R.id.food);
+        food = findViewById(R.id.food_name);
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==submit){
+            title.setText(food.getText().toString());
+            food.setText("");
+        }
+    }
+}
